a1fc0f7f33902c254d6bd53bbfa476b9506c9af7
Henry Pickersgill <htap2@live.co.uk>
2024-04-12 12:21:04 +0100

    fix score being saved as numerical value instead of DX, TX etc

--- utils.py
+++ utils.py
@@ -11,18 +11,30 @@ def calculate_total(d1, d2, d3):
     d1, d2, d3 = convert_score(d1), convert_score(d2), convert_score(d3)
     return d1, d2, d3, sum([int(i) for i in [d1, d2, d3]])
 
+def convert_bull_to_50(darts):
+    new_darts = []
+    for i in darts:
+        if i == "Bull":
+            new_darts.append(50)
+        else:
+            new_darts.append(i)
+    return new_darts
+
 def write_darts_to_file(d1, d2, d3, target):
     csv_file = f"{data_path}/{target}_practice.csv"
     now = datetime.strftime(datetime.now(), "%d/%m/%Y")
+    
+    new_darts = convert_bull_to_50([d1, d2, d3])
+
     if target != "bull":
-        d1, d2, d3, total = calculate_total(d1, d2, d3) 
+        total = calculate_total(d1, d2, d3)[-1]
 
     with open(csv_file, "a") as f:
         if target != "bull":
-            f.write(f"\n{now},{d1},{d2},{d3},{total}")
+            f.write(f"\n{now},{new_darts[0]},{new_darts[1]},{new_darts[2]},{total}")
         else:
             # don't care about total for bullseye practice
-            f.write(f"\n{now},{d1},{d2},{d3}")
+            f.write(f"\n{now},{new_darts[0]},{new_darts[1]},{new_darts[2]}")
 
 def read_3_dart_avg(target, avg=True):
     with open(f"{data_path}/{target}_3_dart_avg.txt", "r") as f:
